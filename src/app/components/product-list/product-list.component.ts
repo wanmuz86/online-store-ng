3d98914cf7ab02613a7528efbda90a2f92e03008
Wan Muzaffar <wanmuz86@gmail.com>
2024-03-26 11:11:31 +0800

    add products from service

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product';
+import { CommonModule } from '@angular/common';
 
+// Import, Inject, Implement
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+  products : Product[] = [];
+
+   //2) Injection - private/public - To call in .ts only/ call in .ts and .html
+   // productService - variable name / ProductService - Type
+   // var productService = new ProductService() 
+  constructor(private productService:ProductService){
+
+  }
+  // 3) Implement / call the function on ngOnInit
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
 
 }
