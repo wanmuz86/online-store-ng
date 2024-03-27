d9c5bc1949f94fd345b4777aaac07b249fea70e0
Wan Muzaffar <wanmuz86@gmail.com>
2024-03-27 09:51:38 +0800

    add navigation through code

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
 
+  constructor(private router: Router) {
+
+  }
+  buttonPressed() {
+    this.router.navigate(['/'])
+  }
 }
