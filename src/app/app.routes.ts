78555b55eda568f8733562d1dedd5d815c20499d
Wan Muzaffar <wanmuz86@gmail.com>
2024-03-27 09:29:30 +0800

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {AboutComponent} from './components/about/about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+export const routes: Routes = [
+    {path:'about', component:AboutComponent},
+    // Default Routing
+    {path:'', component:ProductListComponent},
+    //Wildcard routing
+    {path:'**', component:PageNotFoundComponent}
+];
